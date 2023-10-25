608ffd83fb39f9a118a2147c7d526981c6472fe1
YaelShocht <126409438+YaelShocht@users.noreply.github.com>
2023-10-25 16:19:35 +0300

    Update Lexus.cs
    i delete the constractor

--- Lexus.cs
+++ Lexus.cs
@@ -11,9 +11,7 @@ namespace ConsoleApp2
     {
         public bool IsPlayingMusic { get; private set; } 
         //CR - why not private? also - why do you need a property if you created StartMusic() and StopMusic() for changing the value of this field?
-        public Lexus(string model): base(model) {
-            IsPlayingMusic = false;
-        }
+       
         public void StartMusic()
         {
             IsPlayingMusic = true;
