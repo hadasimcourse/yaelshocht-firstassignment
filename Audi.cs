9611ec218374231aec942d1d4623d441b5e01958
YaelShocht <126409438+YaelShocht@users.noreply.github.com>
2023-10-25 17:19:14 +0300

    Update Audi.cs
    Audi class repairs

--- Audi.cs
+++ Audi.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 //CR - remove all unused packages
 
 namespace ConsoleApp2
 {
     //CR - good! keep using OOP.
     //CR - why do you use internal class?
-    internal class Audi:Car 
+    class Audi:Car 
     {
         public Audi(string model) : base(model) { }
 
